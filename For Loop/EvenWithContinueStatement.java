b9ea8bb44331f7cf203eb9bfa4cb24406e074a54
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-04-05 10:16:09 +0530

    Add files via upload

---
+++ For Loop/EvenWithContinueStatement.java
@@ -0,0 +1,13 @@
+class EvenWithContinueStatement
+{
+public static void main (String args[])
+{ 
+int i;
+for (i=1;i<=10;i++)
+{
+if (i%2==1)
+continue;
+System.out.print(i+"\t");
+}
+}
+} 
---
+++ For Loop/NoSeries.java
@@ -0,0 +1,11 @@
+class NoSeries
+{ 
+public static void main (String args[])
+{
+ int i;
+for (i=1;i<=10;i++)
+{
+ System.out.println(i+"\t");
+}
+}
+}
