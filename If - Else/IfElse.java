beaf78f90f1e7aa1610ba4756a3e29cac53378f9
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2018-04-05 10:16:54 +0530

    Add files via upload

---
+++ If - Else/IfElse.java
@@ -0,0 +1,19 @@
+class IfElse
+ { 
+   public static void main (String args[])
+    {
+      int a=20;
+      if (a>10)
+       {
+        System.out.println("No. is greater");
+       }
+      else if (a<10)
+       { 
+        System.out.println("No. is Smaller");
+       }
+      else
+       {
+        System.out.println("Nos. are equal");
+       }
+    }
+ }
---
+++ If - Else/PrintWordFormatOfNumber.java
@@ -0,0 +1,19 @@
+class PrintWordFormatOfNumber
+ { 
+  public static void main (String args[])
+   {
+     int a= 5;
+      if (a==1)
+      System.out.println("One");
+      else if (a==2)
+      System.out.println("Two");
+      else if (a==3)
+      System.out.println("Three");
+      else if (a==4)
+      System.out.println("Four");
+      else if (a==5)
+      System.out.println("Five");
+        else
+         System.out.println("Invalid No.");
+    }
+  }
