1725b5e7d324e43d0782057e0a60f9d7f909933e
technicalreju <73017717+technicalreju@users.noreply.github.com>
2020-10-18 17:49:15 +0530

    Update DivisorSeries.java

--- Basic Programs/DivisorSeries.java
+++ Basic Programs/DivisorSeries.java
@@ -1,17 +1,15 @@
-class DivisorSeries
- {
-   public static void main (String args[])
+class Test 
+{ 
+    static void printDivisors(int n) 
     { 
-      int i=2;
-      while (i<=10)
-      System.out.println(i+"/"+(i*i)+"\t");
-      i++;
-    }
-      while (i<10)
-      {
-       System.out.print(i+"/"+(i*i)+"+");
-       i++;
-      }
-       System.out.print(i+"/"+(i*i));
-     }
-  }
+        for (int i=1;i<=n;i++) 
+            if (n%i==0) 
+                System.out.print(i+" "); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        System.out.println("The divisors of 100 are: "); 
+        printDivisors(100);; 
+    } 
+} 
